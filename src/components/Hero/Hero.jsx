f7265e9fba0a533624efa83c331db6f4f43b193b
Adam Zawierucha <zawiedev@gmail.com>
2020-07-19 15:58:14 -0700

    comprehensive fix to twitch

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -21,28 +21,31 @@ const Header = () => {
     }
   }, []);
 
-  return (
-    <section id="hero" className="jumbotron">
-      <Container>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
-          <h1 className="hero-title">
-            {title || ''} <span className="text-color-main">{name || ''}</span>
-            <br />
-            {subtitle || ''}
-          </h1>
-        </Fade>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
-          <p className="hero-cta">
-            <span className="cta-btn cta-btn--hero">
-              <Link to="about" smooth duration={1000}>
-                {cta || ''}
-              </Link>
-            </span>
-          </p>
-        </Fade>
-      </Container>
-    </section>
-  );
+  if (name && title && subtitle && cta) {
+    return (
+      <section id="hero" className="jumbotron">
+        <Container>
+          <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
+            <h1 className="hero-title">
+              {title || ''} <span className="text-color-main">{name || ''}</span>
+              <br />
+              {subtitle || ''}
+            </h1>
+          </Fade>
+          <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+            <p className="hero-cta">
+              <span className="cta-btn cta-btn--hero">
+                <Link to="about" smooth duration={1000}>
+                  {cta || ''}
+                </Link>
+              </span>
+            </p>
+          </Fade>
+        </Container>
+      </section>
+    );
+  }
+  return <section id="hero" className="jumbotron" />;
 };
 
 export default Header;
